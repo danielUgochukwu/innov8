d4530f4d4fa348e03f7c27f120bf9597a6fe44f9
Daniel Ugochukwu <slade8546@gmail.com>
2025-02-05 08:56:11 +0100

    use page

--- next.config.ts
+++ next.config.ts
@@ -16,8 +16,8 @@ const nextConfig: NextConfig = {
   devIndicators: {
     appIsrStatus: true,
     buildActivity: true,
-    buildActivityPosition: "bottom-right"
-  }
+    buildActivityPosition: "bottom-right",
+  },
 };
 
 export default nextConfig;
